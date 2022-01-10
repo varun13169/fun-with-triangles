066432baa183692dcd7dee996cdabf50620b5655
Varun Kumar <varun13169@users.noreply.github.com>
2022-01-11 02:20:19 +0530

    Update itatApp.js
    Convert to ES6

--- itatApp.js
+++ itatApp.js
@@ -1,17 +1,16 @@
 
 
-let angleForm = document.querySelector('#form-is-it-a-trangle');
-let angleScoreDisp = document.querySelector('#itat-score-display');
+const angleForm = document.querySelector('#form-is-it-a-trangle');
+const angleScoreDisp = document.querySelector('#itat-score-display');
 
-let angle1 = document.querySelector('#itat-angle1');
-let angle2 = document.querySelector('#itat-angle2');
-let angle3 = document.querySelector('#itat-angle3');
+const angle1 = document.querySelector('#itat-angle1');
+const angle2 = document.querySelector('#itat-angle2');
+const angle3 = document.querySelector('#itat-angle3');
 
-
-function angleOnSubmitHandler(e) {
+const angleOnSubmitHandler = e => {
     e.preventDefault();
 
-    let sumOfAngles = parseInt(angle1.value) + parseInt(angle2.value) + parseInt(angle3.value);
+    const sumOfAngles = parseInt(angle1.value) + parseInt(angle2.value) + parseInt(angle3.value);
     if(sumOfAngles === 180) {
         angleScoreDisp.innerHTML = "Correct";
     }
