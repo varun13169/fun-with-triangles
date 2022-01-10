a0b15f5a6be7f8e60ab1b5cd6f5495a6d0dc5c25
Varun Kumar <varun13169@users.noreply.github.com>
2022-01-11 02:17:45 +0530

    Update hypApp.js
    Convert to ES6

--- hypApp.js
+++ hypApp.js
@@ -1,16 +1,16 @@
 
 
-let hypForm = document.querySelector('#form-calc-hype');
-let hypotenus = document.querySelector('#hypotenuse');
+const hypForm = document.querySelector('#form-calc-hype');
+const hypotenus = document.querySelector('#hypotenuse');
 
-let base = document.querySelector('#hyp-base');
-let height = document.querySelector('#hyp-height');
+const base = document.querySelector('#hyp-base');
+const height = document.querySelector('#hyp-height');
 
 
-function hypOnSubmitHandler(e) {
+const hypOnSubmitHandler = e => {
     e.preventDefault();
 
-    let calHyp = Math.sqrt(Math.pow(parseInt(base.value), 2) + Math.pow(parseInt(height.value), 2));
+    const calHyp = Math.sqrt(Math.pow(parseInt(base.value), 2) + Math.pow(parseInt(height.value), 2));
     hypotenus.innerHTML = "The length of hypotenuse is " + calHyp;
 }
 
